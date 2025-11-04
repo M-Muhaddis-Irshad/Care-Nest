5c90f1c35e675fd066e79af9721942c1d9a1f8ba
M-Muhaddis-Irshad <muhaddisirshad58@gmail.com>
2025-11-04 14:09:14 +0500

    Home page completed

--- app.js
+++ app.js
@@ -1,48 +1,62 @@
-{// Svg's Start's__________________________________________
+{// Svg's Section__________________
 
-    const phoneSvg = document.getElementById('phoneSvg');
-    const mailSvg = document.getElementById('mailSvg');
-    const locationSvg = document.getElementById('locationSvg');
+    {// Svg's Start's__________________________________________
 
-    // Phone Svg_________________________________________
-    fetch('Logo_Icons/icons/sect1Svgs/mobile-phone.svg')
-        .then(res => res.text())
-        .then(data => {
-            phoneSvg.innerHTML = data;
-        });
+        const phoneSvg = document.getElementById('phoneSvg');
+        const mailSvg = document.getElementById('mailSvg');
+        const locationSvg = document.getElementById('locationSvg');
 
-    // Logo_Icons/icons/sect1Svgs/mobile-phone.svg
+        // Phone Svg_________________________________________
+        fetch('Logo_Icons/icons/sect1Svgs/mobile-phone.svg')
+            .then(res => res.text())
+            .then(data => {
+                phoneSvg.innerHTML = data;
+            });
 
-    // Mail Svg_________________________________________
-    fetch('Logo_Icons/icons/sect1Svgs/mail.svg')
-        .then(res => res.text())
-        .then(data => {
-            mailSvg.innerHTML = data;
-        });
+        // Mail Svg_________________________________________
+        fetch('Logo_Icons/icons/sect1Svgs/mail.svg')
+            .then(res => res.text())
+            .then(data => {
+                mailSvg.innerHTML = data;
+            });
 
-    // Logo_Icons/icons/sect1Svgs/mail.svg
+        // Location Svg_________________________________________
+        fetch('Logo_Icons/icons/sect1Svgs/location.svg')
+            .then(res => res.text())
+            .then(data => {
+                locationSvg.innerHTML = data;
+            });
 
-    // Location Svg_________________________________________
-    fetch('Logo_Icons/icons/sect1Svgs/location.svg')
-        .then(res => res.text())
-        .then(data => {
-            locationSvg.innerHTML = data;
-        });
+    }
 
-    // Logo_Icons/icons/sect1Svgs/location.svg
+
+    { // About Cards Svg Start's
+
+        const treatment = document.getElementById('treatment');
+        const starOfLife = document.getElementById('starOfLife');
+        const heartRate = document.getElementById('heartRate');
+        const stethoscope = document.getElementById('stethoscope');
+
+        // Treatment Svg_____________________________
+        fetch("Logo_Icons/icons/aboutSectSvgs/caduceus.svg")
+            .then(res => res.text())
+            .then(data => treatment.innerHTML = data)
+
+        // Star of Life Svg_____________________________
+        fetch("Logo_Icons/icons/aboutSectSvgs/round-star-of-life.svg")
+            .then(res => res.text())
+            .then(data => starOfLife.innerHTML = data)
+
+        // Heart Svg_____________________________
+        fetch("Logo_Icons/icons/aboutSectSvgs/heart-rate.svg")
+            .then(res => res.text())
+            .then(data => heartRate.innerHTML = data)
+
+        // Stethoscope Svg_____________________________
+        fetch("Logo_Icons/icons/aboutSectSvgs/stethoscope.svg")
+            .then(res => res.text())
+            .then(data => stethoscope.innerHTML = data)
+
+    }
 
 }
-
-
-
-
-
-
-
-
-
-
-const treatment = document.getElementById('treatment');
-const starOfLife = document.getElementById('starOfLife');
-const heartRate = document.getElementById('heartRate');
-const stethoscope = document.getElementById('stethoscope');
