e372b73821c80cc7a85cf4cdbc9cdb7e8639019c
M-Muhaddis-Irshad <muhaddisirshad58@gmail.com>
2025-11-10 07:30:11 +0500

    Get data from supabase

--- allPages/appointment/appointment.js
+++ allPages/appointment/appointment.js
@@ -1,7 +1,7 @@
 const supabaseApi = supabase.createClient('https://ubdfphgftdztmmfqoxmf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InViZGZwaGdmdGR6dG1tZnFveG1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxNzgwMDMsImV4cCI6MjA3Nzc1NDAwM30.SxvGOhgEIOTDXNajche0unZy4FfHFocaZVOW3lYh4H0')
 
 const userEmail = localStorage.getItem('userEmail');
-const userName = localStorage.getItem('userName');
+const sessionName = localStorage.getItem('userName');
 
 let stopFunctionFlg = true;
 
@@ -80,20 +80,173 @@ isUserLoggedIn()
 }
 
 
-// Set Inputs value dynamically_____________________________________________
-const usrName = document.getElementById('name').value = userName;
+
+// Set Name Input value dynamically_____________________________________________
+document.getElementById('name').value = sessionName;
+
 
 // Set a Authenticated email of user dynamically direct from session_______________
-    const email = document.getElementById('email');
-    email.value = userEmail;
-    email.disabled = true
+const email = document.getElementById('email');
+email.value = userEmail;
+email.disabled = true;
+
+// Drop Downs_________________________________________
+let doctors = document.getElementById('doctors');
+let date = document.getElementById('date');
+let timing = document.getElementById('timing');
+
+// Drop Downs 1st Value Remove______________________________
+const Val1 = document.querySelectorAll('.val0');
+const optArr = [doctors, date, timing];
+optArr.forEach((optionsValue, i) => {
+    if (optionsValue[i] !== 0) {
+        Val1[i].style.display = "none";
+    }
+})
 
 
-// Get Values
-const doctor = document.getElementById('doctor');
-
-
-// Get Doctor________________________________________________
+// Real Work Start's____________________________________________________
 const appoint = () => {
-    console.log(doctor.value);
+    const nameFromDom = document.getElementById('name').value;
+    if (!nameFromDom.trim()) {
+        return alert("Input Can't be Empty")
+    }
+    const userName = (nameFromDom !== sessionName) ? nameFromDom : sessionName;
+    console.log(userName);
+    console.log(email.value);
+}
+
+
+const retrieve = async () => {
+    const { data, error } = await supabaseApi
+        .from('DoctorsData')
+        .select('Data')
+    if (error) {
+        console.log(error.message)
+        return
+    }
+    let Data = data[0].Data;
+
+    Data.forEach((docs, index) => {
+        const { Doctor: { Days, Name, Timings } } = docs
+        console.log(`
+            Names = ${Name}
+            Days = ${Days}
+            Timings = ${Timings}
+            `)
+        doctors.innerHTML += `<option value="name-${index}">${Name}</option>`
+        date.innerHTML += `<option value="day-${index}">${Days[index]}</option>`
+        timing.innerHTML += `<option value="time-${index}">${Timings}</option>`
+
+    });
+
+}
+
+retrieve()
+
+const doctorsData = [
+    {
+        "Doctor": {
+            "Name": "Dr. Ali",
+            "Days": [
+                "Monoday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday"
+            ],
+            "Timings": ["8:00AM - 1:00PM", "1:00PM - 5:30PM"]
+        }
+    },
+    {
+        "Doctor": {
+            "Name": "Dr. Ayesha",
+            "Days": [
+                "Monoday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday"
+            ],
+            "Timings": ["8:00AM - 1:00PM", "1:00PM - 5:30PM"]
+        }
+    },
+    {
+        "Doctor": {
+            "Name": "Dr. Ahmed",
+            "Days": [
+                "Monoday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday"
+            ],
+            "Timings": ["8:00AM - 1:00PM", "1:00PM - 5:30PM"]
+        }
+    },
+    {
+        "Doctor": {
+            "Name": "Dr. Rehan",
+            "Days": [
+                "Monoday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday"
+            ],
+            "Timings": ["8:00AM - 1:00PM", "1:00PM - 5:30PM"]
+        }
+    },
+    {
+        "Doctor": {
+            "Name": "Dr. Sara",
+            "Days": [
+                "Saturday",
+                "Sunday"
+            ],
+            "Timings": ["9:30AM - 1:00PM", "1:00PM - 5:30PM"]
+        }
+    },
+    {
+        "Doctor": {
+            "Name": "Dr. Salman",
+            "Days": [
+                "Saturday",
+                "Sunday"
+            ],
+            "Timings": ["9:30AM - 1:00PM", "1:00PM - 5:30PM"]
+        }
+    }
+]
+
+// Supabase Insert & Delete Query_______________________________
+{
+    const insert = async () => {
+        const { error } = await supabaseApi
+            .from('DoctorsData')
+            .insert(
+                {
+                    id: 1,
+                    Data: doctorsData
+                }
+            )
+
+        if (error) {
+            console.log(error.message)
+            return
+        }
+    }
+    // insert()
+
+
+    const Delete = async () => {
+        const response = await supabaseApi
+            .from('DoctorsData')
+            .delete()
+            .eq('id', 1)
+        console.log(response);
+
+    }
+
+    // Delete()
 }
