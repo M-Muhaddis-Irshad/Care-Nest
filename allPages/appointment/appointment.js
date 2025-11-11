2039aa6de6311905b81c01ee8004b7131936c0e5
M-Muhaddis-Irshad <muhaddisirshad58@gmail.com>
2025-11-11 17:28:14 +0500

    set the date & time according to selected doctor & all changes are made in BookAppoint.js

--- allPages/appointment/appointment.js
+++ allPages/appointment/appointment.js
@@ -91,9 +91,9 @@ email.value = userEmail;
 email.disabled = true;
 
 // Drop Downs_________________________________________
-let doctors = document.getElementById('doctors');
-let date = document.getElementById('date');
-let timing = document.getElementById('timing');
+const doctors = document.getElementById('doctors');
+const date = document.getElementById('date');
+const timing = document.getElementById('timing');
 
 // Drop Downs 1st Value Remove______________________________
 const Val1 = document.querySelectorAll('.val0');
@@ -116,29 +116,63 @@ const appoint = () => {
     console.log(email.value);
 }
 
+// Array with the same index no. which is show in options for find the exact (Date & Time)______
+const docNamesArr = [];
 
+// Retrieve a session from supabase and set the values on DropDowns_____
 const retrieve = async () => {
     const { data, error } = await supabaseApi
         .from('DoctorsData')
         .select('Data')
+
     if (error) {
         console.log(error.message)
         return
     }
+
     let Data = data[0].Data;
 
+    // Run forEach loop on Data for set the names of doctors in the html <options> tag_____________ 
     Data.forEach((docs, index) => {
-        const { Doctor: { Days, Name, Timings } } = docs
+        const { Doctor: { Name } } = docs
+        docNamesArr.push(Name);
+        doctors.innerHTML += `<option value="${index}">${Name}</option>`;
+
+        // Disable the date & time drop downs before doctor selection_____________________
+        date.disabled = true;
+        timing.disabled = true;
+    });
+
+    doctors.addEventListener('change', event => {
+
+        // console.log(docNamesArr);
+        
+        // Reset the values of (Date & Time) drop downs on every Name change____________________
+        const dateTimeDDowns = [date, timing];
+        dateTimeDDowns.forEach(dDowns => dDowns.options.length = 0)
+        const selectedDoctor = event.target.value
+
+        const { Doctor: { Days, Timings } } = Data[selectedDoctor]
+
         console.log(`
-            Names = ${Name}
+            Name = ${docNamesArr[selectedDoctor]}
             Days = ${Days}
             Timings = ${Timings}
-            `)
-        doctors.innerHTML += `<option value="name-${index}">${Name}</option>`
-        date.innerHTML += `<option value="day-${index}">${Days[index]}</option>`
-        timing.innerHTML += `<option value="time-${index}">${Timings}</option>`
+            `);
 
-    });
+
+        date.disabled = false;
+        timing.disabled = false;
+
+        Days.forEach(days => {
+            date.innerHTML += `<option value="${days}">${days}</option>`
+        })
+
+        Timings.forEach(timings => {
+            timing.innerHTML += `<option value="${timings}">${timings}</option>`
+        })
+
+    })
 
 }
 
@@ -147,7 +181,7 @@ retrieve()
 const doctorsData = [
     {
         "Doctor": {
-            "Name": "Dr. Ali",
+            "Name": ["Dr. Ali"],
             "Days": [
                 "Monoday",
                 "Tuesday",
@@ -160,7 +194,7 @@ const doctorsData = [
     },
     {
         "Doctor": {
-            "Name": "Dr. Ayesha",
+            "Name": ["Dr. Ayesha"],
             "Days": [
                 "Monoday",
                 "Tuesday",
@@ -173,7 +207,7 @@ const doctorsData = [
     },
     {
         "Doctor": {
-            "Name": "Dr. Ahmed",
+            "Name": ["Dr. Ahmed"],
             "Days": [
                 "Monoday",
                 "Tuesday",
@@ -186,7 +220,7 @@ const doctorsData = [
     },
     {
         "Doctor": {
-            "Name": "Dr. Rehan",
+            "Name": ["Dr. Rehan"],
             "Days": [
                 "Monoday",
                 "Tuesday",
@@ -199,7 +233,7 @@ const doctorsData = [
     },
     {
         "Doctor": {
-            "Name": "Dr. Sara",
+            "Name": ["Dr. Sara"],
             "Days": [
                 "Saturday",
                 "Sunday"
@@ -209,7 +243,7 @@ const doctorsData = [
     },
     {
         "Doctor": {
-            "Name": "Dr. Salman",
+            "Name": ["Dr. Salman"],
             "Days": [
                 "Saturday",
                 "Sunday"
@@ -219,8 +253,8 @@ const doctorsData = [
     }
 ]
 
-// Supabase Insert & Delete Query_______________________________
-{
+{// Supabase Insert & Delete Query_______________________________
+
     const insert = async () => {
         const { error } = await supabaseApi
             .from('DoctorsData')
